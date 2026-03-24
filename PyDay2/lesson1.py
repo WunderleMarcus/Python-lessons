5e241dd42b125d2daaf2c59fcb382d6d9751ad59
MarcusWunderle <wunderle@skymail.de>
2026-03-24 10:40:46 +0100

    lesson1 updated

---
+++ PyDay2/lesson1.py
@@ -0,0 +1,69 @@
+#Praktisches Recap von heute: Datentypen, Umwandeln von Datentypen, 
+# Rechnen mit Datentypen, Zugriff auf Elemente
+
+# Aufgabe 1 Datentypen erkennen mit type()
+
+a = 10
+b = 4.14
+c = "Hallo Teilnehmer"
+d = True
+e = [1, 2, 3]
+
+
+
+
+
+
+
+
+
+
+"""
+# Aufgabe 2 Datentypen umwandeln
+
+x = "25"    # in einen Integer umwandeln
+y = 4.8     # in einen Integer umwandeln
+z = 10      # in einen String umwandeln
+
+a = 5
+b = "10"
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Aufgabe 3 Rechnen mit Datentypen: 
+     # Was passiert hier?
+
+    # Lösung:
+print(a + int(b))  
+print(str(a) + b) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Aufgabe 4 Zugriff auf Elemente
+
+text = "Python"
+liste = ["P", "y", "t", "h", "o", "n"]
+
+# Aufgabe:
+# Greife auf das 3. Element zu und gib es aus
+"""
